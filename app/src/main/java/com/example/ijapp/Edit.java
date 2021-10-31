98f8a77b6577d1fc340be3c47d5e42e3bd18bfde
Jwalczaak <Taboret!2345>
2021-10-31 18:16:26 +0100

    edit setting button with edit profile

---
+++ app/src/main/java/com/example/ijapp/Edit.java
@@ -0,0 +1,21 @@
+package com.example.ijapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+public class Edit extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+
+
+    }
+}
--- app/src/main/java/com/example/ijapp/Settings.java
+++ app/src/main/java/com/example/ijapp/Settings.java
@@ -2,13 +2,34 @@ package com.example.ijapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
+
+    private static final String TAG = "Settings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Log.d(TAG, "onCreate: Starting");
+
+        Button btnNavToEdit = (Button) findViewById(R.id.editSettings);
+
+        btnNavToEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClickClicked button editSettings: ");
+
+
+                Intent intent = new Intent(Settings.this, Edit.class);
+                startActivity((intent));
+            }
+        });
     }
 }
